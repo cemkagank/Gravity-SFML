3c3027c90202c852b66c2ded1367635d08daf5ba
Cem Kağan Kayılı <cemkagankayili@hotmail.com>
2024-05-02 15:49:12 +0300

    version 0.1

--- src/gravity.hpp
+++ src/gravity.hpp
@@ -1,7 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <cmath>
 
 class GravitySource {
   sf::Vector2f pos;
@@ -15,6 +17,77 @@ class GravitySource {
       this->str = str;
       s.setPosition(pos);
       s.setFillColor(sf::Color::White);
+      s.setRadius(8);
+    }
+  
+    void render(sf::RenderWindow &window) {
+      window.draw(s);
+    }
+
+    sf::Vector2f get_pos() {
+      return pos;  
+    }
+
+    float get_str(){
+      return str;  
+    }
+
+    
+  private:
+};
+
+class Particle {
+  sf::Vector2f pos;
+  sf::Vector2f vel;
+  sf::CircleShape s;
+
+  public:
+    Particle(float pos_x, float pos_y, float vel_x , float vel_y) {
+      pos.x = pos_x;
+      pos.y = pos_y;
+      vel.x = vel_x;
+      vel.y = vel_y;
+      s.setPosition(pos);
+      s.setFillColor(sf::Color::Green);
+      s.setRadius(5);
+    }
+  
+    void render(sf::RenderWindow &window) {
+      s.setPosition(pos);
+      window.draw(s);
+    }
+
+    sf::Vector2f get_pos() {
+      return pos;  
+    }
+
+    sf::Vector2f get_vel(){
+      return vel;  
+    }
+
+    void update_pyhsics(GravitySource &gs) {
+      float dx = gs.get_pos().x - pos.x;
+      float dy = gs.get_pos().y - pos.y;
+      float dc = sqrt(dx * dx + dy * dy);
+      
+      float ic = 1.f / dc;
+      float ic2 = ic * ic;
+
+      float nx = dx * ic;
+      float ny = dy * ic;
+
+      float acc_x = nx * gs.get_str() * ic2 ;
+      float acc_y = ny * gs.get_str() * ic2 ;
+
+      vel.x += acc_x;
+      vel.y += acc_y;
+
+      pos.x += vel.x;
+      pos.y += vel.y;
+    }
+    
+    void set_color(sf::Color c) {
+      s.setFillColor(c);
     }
 
     
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,62 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
+#include "gravity.hpp"
+#include <cmath>
+#include <vector>
+
+#define WIDTH 1366
+#define HEIGHT 768
+#define FPS 120
+#define CENTER_W (float)WIDTH/2
+#define CENTER_H (float)HEIGHT/2
+
+
+// TODO: add mouse event to add particle or power source 
+// NOTE: new system to detect and attach particles to power sources.
+// TODO: add sliders to change str of power source
+
+
+sf::Color interpolate(float val){
+  int r,g,b = 0;
+  val = fmod(val, 1.0);
+  if (val < 0.5f ) {
+    b = 255 * (1.0f - 2 * val);
+    g = 255 * 2 * val; 
+  } else {
+    g = 255 * (2.0f - 2 * val);
+    r = 255 * (2 * val -1);
+  }
+  return sf::Color(r,g,b);
+}
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(1280, 720), "Gravity");
-  window.setFramerateLimit(120);
+  sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Gravity");
+  window.setFramerateLimit(FPS);
+  
+  GravitySource s1 = GravitySource(CENTER_W - 500, CENTER_H,3000);
+  GravitySource s2 = GravitySource(CENTER_W + 500,CENTER_H,3000);
+  GravitySource s3 = GravitySource(CENTER_W,160,3000);
+  
+
+  std::vector<GravitySource> sources;
+  sources.push_back(s1);
+  sources.push_back(s2);
+  sources.push_back(s3);
+
+  const int num_particle = 200;
+  std::vector<Particle> particles;
+  
+  for (int i =0 ; i < num_particle; i++) {
+    particles.push_back(Particle(CENTER_W + 3,260,4, 0.2 + (0.1/ num_particle) * i ));
+    float val = (float)i / (float)num_particle;
+    sf::Color col = interpolate(val);
+    particles[i].set_color(col);
+
+  }
+
+
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
@@ -12,7 +64,19 @@ int main() {
         window.close();
       }
     }
+
     window.clear();
+
+    for (int i=0 ; i < sources.size(); i++) {
+      for (int j = 0; j < particles.size(); j++) {
+        particles[j].update_pyhsics(sources[i]);
+      }
+    
+    }
+
+    for (auto s: sources) s.render(window);
+    for (auto p: particles) p.render(window);
+
     window.display();
   }
 }
